0652cfd85fc631ba268537102cad7edf430e9513
Grant Gorham <grantegorham26@gmail.com>
2024-04-29 20:49:41 -0600

    date to date time data type in sql schema

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE category (
-	category_id VARCHAR NOT NULL,
+	category_id VARCHAR(25) NOT NULL,
 	category VARCHAR(25) NOT NULL,
 	PRIMARY KEY (category_id)
 );
 
 CREATE TABLE subcategory (
-	subcategory_id VARCHAR NOT NULL,
+	subcategory_id VARCHAR(25) NOT NULL,
 	subcategory VARCHAR(25) NOT NULL,
 	PRIMARY KEY (subcategory_id)
 );
@@ -23,16 +23,16 @@ CREATE TABLE campaign (
 	contact_id INT NOT NULL,
 	company_name VARCHAR(50) NOT NULL,
 	description VARCHAR(200),
-	goal NUMERIC,
-	pledged NUMERIC,
+	goal FLOAT,
+	pledged FLOAT,
 	outcome VARCHAR(20),
 	backers_count INT,
-	country VARCHAR(2),
-	currency VARCHAR(3),
-	launch_date DATE,
-	end_date DATE,
-	category_id VARCHAR NOT NULL,
-	subcategory_id VARCHAR NOT NULL,
+	country VARCHAR(10),
+	currency VARCHAR(10),
+	launch_date DATETIME,
+	end_date DATETIME,
+	category_id VARCHAR(25) NOT NULL,
+	subcategory_id VARCHAR(25) NOT NULL,
 	PRIMARY KEY (cf_id),
 	FOREIGN KEY (contact_id) REFERENCES contacts (contact_id),
 	FOREIGN KEY (category_id) REFERENCES category (category_id),
