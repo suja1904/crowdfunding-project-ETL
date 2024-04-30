956cde3fb2e3fcee5d733a2b7af8bce00dda30e9
Grant Gorham <grantegorham26@gmail.com>
2024-04-29 20:17:48 -0600

    formatted

--- crowdfunding_db_verification.sql
+++ crowdfunding_db_verification.sql
@@ -1,24 +1,51 @@
-SELECT * FROM campaign
-LIMIT 10;
-SELECT * FROM category
-LIMIT 10;
-SELECT * FROM subcategory
-LIMIT 10;
-SELECT * FROM contacts
-LIMIT 10;
+SELECT
+	*
+FROM
+	campaign
+LIMIT
+	10;
+
+SELECT
+	*
+FROM
+	category
+LIMIT
+	10;
+
+SELECT
+	*
+FROM
+	subcategory
+LIMIT
+	10;
+
+SELECT
+	*
+FROM
+	contacts
+LIMIT
+	10;
 
 --Display Key values and group by Currency in order of pledge highest to lowest 
 --validate the category and subcategory are correct
-
-SELECT 
-company_name, currency, pledged, outcome, end_date, 
-b.category, c.subcategory,
-first_name, last_name, email
-
-FROM  campaign a 
-			LEFT JOIN category b on a.category_id = b.category_id
-			LEFT JOIN subcategory c on a.subcategory_id = c.subcategory_id
-			LEFT JOIN contacts d on a.contact_id = d.contact_id
-
-WHERE outcome IN ('successful')
-ORDER BY Currency, pledged DESC
+SELECT
+	company_name,
+	currency,
+	pledged,
+	outcome,
+	end_date,
+	b.category,
+	c.subcategory,
+	first_name,
+	last_name,
+	email
+FROM
+	campaign a
+	LEFT JOIN category b on a.category_id = b.category_id
+	LEFT JOIN subcategory c on a.subcategory_id = c.subcategory_id
+	LEFT JOIN contacts d on a.contact_id = d.contact_id
+WHERE
+	outcome IN ('successful')
+ORDER BY
+	Currency,
+	pledged DESC
